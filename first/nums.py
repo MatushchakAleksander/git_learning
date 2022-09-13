e4a6aaaac01384ef083e96508ab0b4d84abf3e2e
MatushchakAleksander <112856861+MatushchakAleksander@users.noreply.github.com>
2022-09-13 15:44:31 +0300

    Добавлена функция div

--- first/nums.py
+++ first/nums.py
@@ -8,7 +8,14 @@ def minus(a, b):
 def mul (a, b):
     return a*b
 
+def div (a, b):
+    return a/b
+
+
+
+
 if __name__=='__main__':
     print(plus(2, 2))
     print(minus(4, 2))
     print(mul(3, 3))
+    print(div(24, 3))
